a8bcee79c973e415ebf6386c16fc1b1c444172d7
ROSTAND LELE <rostoinfocus@gmail.com>
2025-11-26 21:36:02 +0100

    link app .env

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -3,14 +3,15 @@
 namespace App\Filament\Resources\Posts\Schemas;
 
 use App\Models\Category;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Components\DatePicker;
-use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\MarkdownEditor;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Schemas\Schema;
 
 
@@ -20,16 +21,24 @@ class PostForm
     {
         return $schema
             ->components([
-               TextInput::make("title"),
+               TextInput::make("title")
+               ->required(),
                TextInput::make("slug"),
                Select::make("category_id")
-                            ->label("Category")
-                            ->options(Category::all()->pluck("name", "id")),
-                ColorPicker::make("color"),
-                MarkdownEditor::make('body'),
-                FileUpload::make("image")->disk("public")->directory("posts"),
-                TagsInput::make("tags"),            
-                Checkbox::make("published"), 
+               ->label("Category")
+               ->options(Category::all()->pluck("name", "id"))
+               ->required(),
+               ColorPicker::make("color"),
+               MarkdownEditor::make('body')
+               ->required(),
+               FileUpload::make("image")
+               ->image()
+               ->disk("public")
+               ->directory("posts")
+               ->nullable(),
+                TagsInput::make("tags")
+                ->required(),            
+                Toggle::make("published"), 
                 DatePicker::make("published_at"),           
             ]);
     }
--- app/Filament/Resources/Posts/Tables/PostsTable.php
+++ app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -5,9 +5,11 @@ namespace App\Filament\Resources\Posts\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Forms\Components\Toggle;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Table;
 
 class PostsTable
@@ -16,12 +18,14 @@ class PostsTable
     {
         return $table
             ->columns([
-                ImageColumn::make("image")->disk("public")
-                ->url(fn ($record) => asset('storage/' . $record->image)),
+                ImageColumn::make("image")
+                 ->circular()
+                ->disk("public"),
                 TextColumn::make("title"),
                 TextColumn::make("slug"),
                 TextColumn::make("category.name"),
                 TextColumn::make("body"),
+                ToggleColumn::make("published"),
                 ColorColumn::make('color'),
                 TextColumn::make("tags"),
             ])
