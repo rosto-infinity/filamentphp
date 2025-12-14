97034c112d122a0d228256026de0146e019e0c0c
ROSTAND LELE <rostoinfocus@gmail.com>
2025-12-14 15:44:26 +0100

    city_control 70%

--- app/Filament/Resources/Cities/Schemas/CityForm.php
+++ app/Filament/Resources/Cities/Schemas/CityForm.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Cities\Schemas;
 
 use App\Models\Country;
+use App\Models\State;
 use Filament\Schemas\Schema;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
@@ -14,11 +15,20 @@ class CityForm
     {
         return $schema
             ->components([
-                   Select::make("country_id")
-                  ->options(Country::pluck("name", "id")),
-                   Select::make("state_id")
-                  ->relationship("state", "name"),
-                TextInput::make("name"),    
+                Select::make("country_id")
+                    ->options(Country::pluck("name", "id"))->reactive(),
+                Select::make("state_id")
+                    ->label("Nom de la region")
+                    ->options(function (callable $get) {
+                        $country = $get("country_id");
+                        if (!$country) {
+                            return [];
+                        } else {
+                            return State::whereCountryId($country)
+                                ->pluck("name", "id");
+                        }
+                    })->reactive(),
+                TextInput::make("name"),
             ]);
     }
 }
